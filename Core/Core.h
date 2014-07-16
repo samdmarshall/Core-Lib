bdfe7ff81b9eef3dad115cae38994f121cd54811
Jeremy Agostino <jeremyagost@gmail.com>
2014-07-16 18:56:52 -0400

    Add option to use header include paths relative to an external project (__SDM_CORE_LIB)

--- Core/Core.h
+++ Core/Core.h
@@ -9,6 +9,38 @@
 #ifndef Core_Core_h
 #define Core_Core_h
 
+#if __SDM_CORE_LIB
+
+/*
+ * Indicates that Core-Lib is being provided as a standalone library
+ * and headers should be accessed in the exported location.
+ */
+
+#include <SDMCore/Pointer.h>
+#include <SDMCore/Range.h>
+#include <SDMCore/Number.h>
+#include <SDMCore/Logging.h>
+#include <SDMCore/Attributes.h>
+#include <SDMCore/Buffer.h>
+#include <SDMCore/Array.h>
+#include <SDMCore/Endian.h>
+#include <SDMCore/File.h>
+
+#ifdef __COREGEOMETRY__
+#include <SDMCore/Geometry/GeometryMaths.h>
+#include <SDMCore/Geometry/Vector.h>
+#include <SDMCore/Geometry/Matrix.h>
+#endif
+
+#ifdef __COREFOUNDATION_COREFOUNDATION__
+#include <SDMCore/CoreFoundation/CFDictionaryAddition.h>
+#include <SDMCore/CoreFoundation/CFTypeAddition.h>
+#include <SDMCore/CoreFoundation/CFDataAddition.h>
+#include <SDMCore/CoreFoundation/CFStringAddition.h>
+#endif
+
+#else
+
 #pragma mark -
 #pragma mark Standard
 #include "Pointer.h"
@@ -38,5 +70,6 @@
 #include "CFStringAddition.h"
 #endif
 
+#endif // __SDM_CORE_LIB
 
 #endif
